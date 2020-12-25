f0d10e22b1ca7dd996ab4b268ca14f1f667e41b6
Korchagin Egor <korchagin.egorka1@gmail.com>
2020-12-25 18:18:07 +1000

    update

--- ModelPicker/ContentView.swift
+++ ModelPicker/ContentView.swift
@@ -8,32 +8,192 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
+import FocusEntity
+
 
 struct ContentView : View {
+    @State private var isPlacementEnabled = false
+    @State private var selectedModel: Model?
+    @State private var modelConfirmForPlacement: Model?
+    
+    
+    private var models: [Model] = {
+        //Dynamically get our model filename
+        let filemanager = FileManager.default
+        
+        guard let path = Bundle.main.resourcePath, let
+        files = try?
+            filemanager.contentsOfDirectory(atPath: path) else {
+                return[]
+        }
+        
+        var avialableModels: [Model] = []
+        for filename in files where
+            filename.hasSuffix("usdz") {
+                let modelName = filename.replacingOccurrences(of: ".usdz", with: "")
+                
+                let model = Model(modelName: modelName)
+                
+                avialableModels.append(model)
+        }
+        return avialableModels
+    }()
+    
     var body: some View {
-        return ARViewContainer().edgesIgnoringSafeArea(.all)
+        ZStack(alignment: .bottom) {
+            ARViewContainer(modelConfirmForPlacement: self.$modelConfirmForPlacement)
+            
+            if self.isPlacementEnabled {
+                PlacementButtonsView(isPlacementEnabled: self.$isPlacementEnabled, selectedModel: self.$selectedModel, modelConfirmForPlacement: self.$modelConfirmForPlacement)
+            } else {
+                ModelPickerView(isPlacementEnabled: self.$isPlacementEnabled, selectedModel: self.$selectedModel, models: self.models)
+            }
+            //PlacementButtonsView()
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
-    
+    @Binding var modelConfirmForPlacement: Model?
     func makeUIView(context: Context) -> ARView {
         
-        let arView = ARView(frame: .zero)
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        let arView = CustomARView(frame: .zero)//ARView(frame: .zero)
         
         return arView
         
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func updateUIView(_ uiView: ARView, context: Context) {
+        if let model = self.modelConfirmForPlacement {
+            
+            if let modelEntity = model.modelEntity {
+                print("DEBUG: adding model to scene -\(model.modelName)")
+                
+                let anchorEntity = AnchorEntity(plane: .any)
+                anchorEntity.addChild(modelEntity.clone(recursive: true))
+                
+                uiView.scene.addAnchor(anchorEntity)
+            } else {
+                print("DEBUG: Unable to load modelEntity for \(model.modelName)")
+
+                
+            }
+            
+                DispatchQueue.main.async {
+                self.modelConfirmForPlacement = nil
+            }
+        }
+    }
     
 }
+    class CustomARView: ARView, FEDelegate {
+           let focusSquare = FESquare()
+           required init(frame frameRect: CGRect) {
+               super.init(frame: frameRect)
+               
+               focusSquare.viewDelegate = self
+               focusSquare.delegate = self
+               focusSquare.setAutoUpdate(to: true)
+            
+                self.setupARView()
+           }
+        
+        @objc required dynamic init?(coder decoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        func setupARView() {
+          let config = ARWorldTrackingConfiguration()
+          config.planeDetection = [.horizontal, .vertical]
+          config.environmentTexturing = .automatic
+          
+          if ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh) {
+              config.sceneReconstruction = .mesh
+              
+          }
+          
+          self.session.run(config)
+        }
+    }
+    
+
+
+
+struct ModelPickerView: View {
+    @Binding var isPlacementEnabled: Bool
+    @Binding var selectedModel: Model?
+    
+    var models: [Model]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 30) {
+                    ForEach(0 ..< self.models.count) {
+                        index in
+                        Button(action: {
+                            print("DEBUG: selected model wirh name:\(self.models[index].modelName)")
+                            
+                            self.selectedModel = self.models[index]
+                            
+                            self.isPlacementEnabled = true
+                        }) {
+                            Image(uiImage: self.models[index].image)
+                                .resizable()
+                                .frame(height: 80)
+                                .aspectRatio(1/1,contentMode: .fit)
+                                .background(Color.white)
+                            .cornerRadius(12)
+                        }
+                    .buttonStyle(PlainButtonStyle())
+                    }
+                }
+            }
+        .padding(20)
+            .background(Color.black.opacity(0.5))
+    }
+}
+    
+    struct PlacementButtonsView: View {
+        @Binding var isPlacementEnabled: Bool
+        @Binding var selectedModel: Model?
+        @Binding var modelConfirmForPlacement: Model?
+        
+        var body: some View {
+            HStack {
+                    // Cancel Button
+                    Button(action: {
+                        print("DEBUG: Cancel model canceled.")
+                        self.resetPlacemenrParameters()
+                    }) {
+                        Image(systemName: "xmark")
+                            .frame(width: 60, height: 60)
+                            .font(.title)
+                            .background(Color.white.opacity(0.75))
+                            .cornerRadius(30)
+                            .padding(20)
+                    }
+                    // Confirm Button
+                    Button(action: {
+                        print("DEBUG: Model placement confirmed.")
+                        
+                        self.modelConfirmForPlacement = self.selectedModel
+                        
+                        self.resetPlacemenrParameters()
+                    }) {
+                        Image(systemName: "checkmark")
+                            .frame(width: 60, height: 60)
+                            .font(.title)
+                            .background(Color.white.opacity(0.75))
+                            .cornerRadius(30)
+                            .padding(20)
+                }
+            }
+        }
+        
+        func resetPlacemenrParameters() {
+            self.isPlacementEnabled = false
+            self.selectedModel = nil
+        }
+    }
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
@@ -42,3 +202,4 @@ struct ContentView_Previews : PreviewProvider {
     }
 }
 #endif
+}
--- ModelPicker/Model.swift
+++ ModelPicker/Model.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2020 Egor Korchagin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import RealityKit
+import Combine
+
+
+class Model  {
+    var modelName: String
+    var image: UIImage
+    var modelEntity: ModelEntity?
+    
+    private var cancellable: AnyCancellable? = nil
+    
+    init(modelName: String) {
+        self.modelName = modelName
+        
+        self.image = UIImage(named: modelName)!
+        
+        let fileName = modelName + ".usdz"
+        self.cancellable = ModelEntity.loadModelAsync(named: fileName).sink(receiveCompletion: { (loadCompletion) in
+            //Handle our error
+            print("DEBUG: Unable to load modelEntity for modelName: \(self.modelName)")
+        }, receiveValue: { modelEntity in
+            //Get our modelEntity
+            self.modelEntity = modelEntity
+            print("DEBUG: Successfully loaded modelEntity for modelName: \(self.modelName) ")
+        })
+        
+        
+    }
+}
